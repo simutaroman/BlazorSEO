161f3ba3025705c6872328770a8e62a357489203
Roman Simuta <simuta@edsson.com>
2020-10-14 14:53:08 +0300

    basic seo componetn and service

---
+++ SeoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSEO
+{
+    public class SeoService
+    {
+        private string currentUri;
+
+        public string CurrentUri { 
+            get => currentUri;
+            set
+            {
+                currentUri = value;
+                OnChanged();
+            } 
+        }
+
+        public event Action OnChanged;
+    }
+}
